624f8bdf8fa89f15ca042d27c2b6f801d7da7bfb
EduardBoboc <edi.boboc33@gmail.com>
2020-12-23 00:08:38 +0200

    Încărcare proiect
    Încărcare proiect cu o primă fază a butonului personalizabil - șablonul. Va trebui ca valorile (textul, culoarea textului și a fundalului și sursa imaginii) să se poată schimba în mod dinamic.

---
+++ src/CustomButton/CustomButton/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CustomButton
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
